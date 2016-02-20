dd1606d076cc2492c1d38ee5ab83b9bbdc67c30f
rkerber <rkerber@users.noreply.github.com>
2016-02-20 12:23:22 -0600

    1.8.9 Pre-Release
    Added in Costura.Fody to remove extra dll files.

--- letsencrypt-win-simple/Properties/AssemblyInfo.cs
+++ letsencrypt-win-simple/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.8.8.*")]
-[assembly: AssemblyFileVersion("1.8.8.8")]
+[assembly: AssemblyVersion("1.8.9.*")]
+[assembly: AssemblyFileVersion("1.8.9.1")]
