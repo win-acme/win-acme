ac2cbea41c17af9c9fa2e19c8b6091830ad0b61e
rkerber <rkerber@users.noreply.github.com>
2016-03-14 18:49:12 -0500

    Version Bump

--- letsencrypt-win-simple/Properties/AssemblyInfo.cs
+++ letsencrypt-win-simple/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.8.9.*")]
-[assembly: AssemblyFileVersion("1.8.9.2")]
+[assembly: AssemblyVersion("1.9.0.*")]
+[assembly: AssemblyFileVersion("1.9.0.1")]
