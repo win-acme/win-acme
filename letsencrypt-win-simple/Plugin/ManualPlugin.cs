657e4a6bffad9f9984da4c1ca146c0a200e72483
John Marden <john@tlc-direct.co.uk>
2016-09-26 11:56:25 +0100

    Created --San --ManualHost mode support w/ ManualHost Renewal support.

--- letsencrypt-win-simple/Plugin/ManualPlugin.cs
+++ letsencrypt-win-simple/Plugin/ManualPlugin.cs
@@ -21,7 +21,28 @@ namespace LetsEncrypt.ACME.Simple
         public override List<Target> GetSites()
         {
             var result = new List<Target>();
+            string[] lsDomains = new string[0];
+            Target loTemp = new Target();
+            
+            if (Program.Options.San && !string.IsNullOrEmpty(Program.Options.ManualHost)) {
+                lsDomains = Program.Options.ManualHost.Split(',');
+                if (!(lsDomains == null) && lsDomains.Length <= 100) {
+                    loTemp = new Target() {
+                        Host = lsDomains[0],
+                        WebRootPath = Program.Options.WebRoot,
+                        PluginName = Name,
+                        AlternativeNames = new List<string>(lsDomains)
+                    };
+                    result.Add(loTemp);
+                    loTemp = null;
 
+                } else {
+                    Console.WriteLine(
+                        $" You must specify at least one (but not more than 100) hosts for a SAN certificate. Let's Encrypt currently has a maximum of 100 alternative names per certificate.");
+                    Log.Error(
+                        "You must specify at least one (but not more than 100) hosts for a SAN certificate. Let's Encrypt currently has a maximum of 100 alternative names per certificate.");
+                }
+            }
             return result;
         }
 
@@ -73,26 +94,45 @@ namespace LetsEncrypt.ACME.Simple
             }
         }
 
-        public override void Renew(Target target)
-        {
-            Console.WriteLine(" WARNING: Unable to renew.");
+        public override void Renew(Target target) {
+            string[] lsDomains = new string[0];
+
+            target.Valid = false;
+            if (Program.ManualSanMode) {
+                if (target.WebRootPath == Program.Options.WebRoot) {
+                    lsDomains = Program.Options.ManualHost.Split(',');
+                    if (lsDomains.Length > 0 && lsDomains.Length <= 100) {
+                        //Check that they're the same domains
+                        target.Valid = (target.Host == lsDomains[0] &&
+                            string.Join(",", target.AlternativeNames.ToArray()) == Program.Options.ManualHost);
+                    }
+                }
+
+            } else {
+                //Check the renewal relates to the CLI's host and webroot
+                target.Valid = (target.Host == Program.Options.ManualHost &&
+                                    target.WebRootPath == Program.Options.WebRoot);
+            }
+            if (target.Valid ) {
+                Console.WriteLine($" Processing Manual Certificate Renewal...");
+                this.Auto(target);
+            }
         }
 
-        public override void PrintMenu()
-        {
-            if (!String.IsNullOrEmpty(Program.Options.ManualHost))
-            {
-                var target = new Target()
-                {
-                    Host = Program.Options.ManualHost,
-                    WebRootPath = Program.Options.WebRoot,
-                    PluginName = Name
-                };
-                Auto(target);
-                Environment.Exit(0);
-            }
+        public override void PrintMenu() {
+            if (!Program.Options.San) {
+                if (!String.IsNullOrEmpty(Program.Options.ManualHost)) {
+                    var target = new Target() {
+                        Host = Program.Options.ManualHost,
+                        WebRootPath = Program.Options.WebRoot,
+                        PluginName = Name
+                    };
+                    Auto(target);
+                    Environment.Exit(0);
+                }
 
-            Console.WriteLine(" M: Generate a certificate manually.");
+                Console.WriteLine(" M: Generate a certificate manually.");
+            }
         }
 
         public override void HandleMenuResponse(string response, List<Target> targets)
@@ -113,7 +153,8 @@ namespace LetsEncrypt.ACME.Simple
                     Stream inputStream = Console.OpenStandardInput(BufferSize);
                     Console.SetIn(new StreamReader(inputStream, Console.InputEncoding, false, BufferSize));
 
-                    var sanInput = Console.ReadLine();
+                    // Include host in the list of DNS names passed to LE
+                    var sanInput = hostName + "," + Console.ReadLine();
                     alternativeNames = sanInput.Split(',');
                     sanList = new List<string>(alternativeNames);
                 }
