e6eaf365e4df00a83e40165acbccef8022cd1705
Sebastiaan Janssen <sebastiaan@umbraco.com>
2017-03-19 11:39:25 +0100

    Bump version

--- letsencrypt-win-simple/Properties/AssemblyInfo.cs
+++ letsencrypt-win-simple/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+// [assembly: AssemblyVersion("1.9.3.*")]
 
-[assembly: AssemblyVersion("1.9.2.*")]
-[assembly: AssemblyFileVersion("1.9.2.0")]
+[assembly: AssemblyVersion("1.9.3.*")]
+[assembly: AssemblyFileVersion("1.9.3.0")]
