f492a063ff8abf55a9031a59ea624c7ca8587d3c
WouterTinus <wouter.tinus@gmail.com>
2017-08-21 19:48:21 +0200

    Preserve program output when running in --test mode

--- letsencrypt-win-simple/Program.cs
+++ letsencrypt-win-simple/Program.cs
@@ -157,7 +157,7 @@ namespace LetsEncrypt.ACME.Simple
                     Log.Error("Exception {@e}", e.Message);
                 }
 
-                if (!Options.Renew && !Options.CloseOnFinish)
+                if (Options.Test || (!Options.Renew && !Options.CloseOnFinish))
                 {
                     if (Input.PromptYesNo("Would you like to start again?"))
                     {
