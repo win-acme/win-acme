c9f465a3929fe5d5f5c743c14a080fa97a2352e9
WouterTinus <wouter.tinus@gmail.com>
2017-09-13 06:10:08 +0200

    Bump version and build without Prefer32bit

--- letsencrypt-win-simple/Properties/AssemblyInfo.cs
+++ letsencrypt-win-simple/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.9.4.*")]
 
-[assembly: AssemblyVersion("1.9.4.*")]
-[assembly: AssemblyFileVersion("1.9.4.0")]
+[assembly: AssemblyVersion("1.9.5.*")]
+[assembly: AssemblyFileVersion("1.9.5.0")]
