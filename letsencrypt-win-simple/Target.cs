59988f077b52643cd794d229b7f96cd4eefb940c
WouterTinus <wouter.tinus@gmail.com>
2017-10-02 22:08:13 +0200

    Add comments

--- letsencrypt-win-simple/Target.cs
+++ letsencrypt-win-simple/Target.cs
@@ -16,28 +16,90 @@ namespace LetsEncrypt.ACME.Simple
 {
     public class Target
     {
+        /// <summary>
+        /// Friendly name of the certificate, which may or may
+        /// no also be the common name (first host), as indicated
+        /// by the <see cref="HostIsDns"/> property.
+        /// </summary>
         public string Host { get; set; }
+
+        /// <summary>
+        /// Is the name of the certificate also a DNS identifier?
+        /// </summary>
         public bool? HostIsDns { get; set; }
-        public bool? Hidden { get; set; }
+
+        /// <summary>
+        /// Hide target from the user in interactive mode, i.e.
+        /// because some filter has been applied (--hidehttps).
+        /// </summary>
+        [JsonIgnore] public bool? Hidden { get; set; }
+
+        /// <summary>
+        /// Triggers IIS specific behaviours, such as copying
+        /// the web.config file in case of Http validation
+        /// </summary>
         public bool? IIS { get; set; }
+
+        /// <summary>
+        /// Path to use for Http validation (may be local or remote)
+        /// </summary>
         public string WebRootPath { get; set; }
+
+        /// <summary>
+        /// Identify the IIS website that the target is based on
+        /// </summary>
         public long SiteId { get; set; }
+
+        /// <summary>
+        /// List of bindings to exclude from the certificate
+        /// </summary>
         public string ExcludeBindings { get; set; }
+
+        /// <summary>
+        /// List of alternative names for the certificate
+        /// </summary>
         public List<string> AlternativeNames { get; set; } = new List<string>();
 
-        public string PluginName { get; set; } = IISClient.PluginName;
-        public string ValidationPluginName { get; set; }
+        /// <summary>
+        /// Name of the plugin to use for refreshing the target
+        /// </summary>
         public string TargetPluginName { get; set; }
 
-        // Plugin specific options
-        public AzureDnsOptions DnsAzureOptions { get; set; }
-        public DnsScriptOptions DnsScriptOptions { get; set; }
+        /// <summary>
+        /// Name of the plugin to use for validation
+        /// </summary>
+        public string ValidationPluginName { get; set; }
+
+        /// <summary>
+        /// Options for ValidationPlugins.Http.Ftp
+        /// </summary>
         public FtpOptions HttpFtpOptions { get; set; }
+
+        /// <summary>
+        /// Options for ValidationPlugins.Http.WebDav
+        /// </summary>
         public WebDavOptions HttpWebDavOptions { get; set; }
 
-        // Legacy (future installer plugins)
+        /// <summary>
+        /// Options for ValidationPlugins.Dns.Azure
+        /// </summary>
+        public AzureDnsOptions DnsAzureOptions { get; set; }
+
+        /// <summary>
+        /// Options for ValidationPlugins.Dns.Script
+        /// </summary>
+        public DnsScriptOptions DnsScriptOptions { get; set; }
+
+        /// <summary>
+        /// Installer plugin
+        /// </summary>
+        public string PluginName { get; set; } = IISClient.PluginName;
         [JsonIgnore] public Plugin Plugin => Program.Plugins.GetByName(Program.Plugins.Legacy, PluginName);
 
+        /// <summary>
+        /// Pretty print information about the target
+        /// </summary>
+        /// <returns></returns>
         public override string ToString() {
             var x = new StringBuilder();
             x.Append($"[{PluginName}] ");
@@ -73,6 +135,10 @@ namespace LetsEncrypt.ACME.Simple
             return x.ToString();
         }
 
+        /// <summary>
+        /// Parse list of excluded hosts
+        /// </summary>
+        /// <returns></returns>
         public List<string> GetExcludedHosts()
         {
             var exclude = new List<string>();
@@ -83,6 +149,13 @@ namespace LetsEncrypt.ACME.Simple
             return exclude;
         }
 
+        /// <summary>
+        /// Parse unique DNS identifiers that the certificate should be 
+        /// created for, taking into account the list of exclusions,
+        /// support for IDNs and the limits of Let's Encrypt
+        /// </summary>
+        /// <param name="unicode"></param>
+        /// <returns></returns>
         public List<string> GetHosts(bool unicode)
         {
             var hosts = new List<string>();
