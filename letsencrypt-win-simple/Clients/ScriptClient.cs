cb0ab943cf321358574cb933c1fa1db0c77bfdbf
WouterTinus <wouter.tinus@gmail.com>
2017-10-15 10:22:34 +0200

    Restructure script running code

--- letsencrypt-win-simple/Clients/ScriptClient.cs
+++ letsencrypt-win-simple/Clients/ScriptClient.cs
@@ -15,21 +15,21 @@ namespace LetsEncrypt.ACME.Simple.Clients
         public static void RunScript(string script, string parameterTemplate, params string[] parameters)
         {
             Process process = null;
-            if (!string.IsNullOrWhiteSpace(script) && !string.IsNullOrWhiteSpace(parameterTemplate))
+            if (!string.IsNullOrWhiteSpace(script))
             {
-                var parametersFormat = string.Format(parameterTemplate, parameters);
-                Program.Log.Information(true, "Running {script} with {parameters}", script, parametersFormat);
-                process = Process.Start(script, parametersFormat);
+                if (!string.IsNullOrWhiteSpace(parameterTemplate))
+                {
+                    var parametersFormat = string.Format(parameterTemplate, parameters);
+                    Program.Log.Information(true, "Running {script} with {parameters}", script, parametersFormat);
+                    process = Process.Start(script, parametersFormat);
+                }
+                else 
+                {
+                    Program.Log.Information(true, "Running {script}", script);
+                    process = Process.Start(script);
+                }
             }
-            else if (!string.IsNullOrWhiteSpace(script))
-            {
-                Program.Log.Information(true, "Running {script}", script);
-                process = Process.Start(script);
-            }
-            else
-            {
-                Program.Log.Warning("Unable to configure server software.");
-            }
+        
             if (process != null)
             {
                 process.WaitForExit();
@@ -38,7 +38,10 @@ namespace LetsEncrypt.ACME.Simple.Clients
                     Program.Log.Warning("Process finished with exitcode {code}", process.ExitCode);
                 }
             }
-            
+            else
+            {
+                Program.Log.Warning("Unable to run script.");
+            }
         }
 
         public override void Install(Target target, string pfxFilename, X509Store store, X509Certificate2 newCertificate, X509Certificate2 oldCertificate)
