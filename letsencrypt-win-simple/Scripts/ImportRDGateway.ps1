0b515681856a38b0eec0f48497636472c9e78fa0
Les Begnaud <lbegnaud@radersolutions.com>
2017-11-10 14:54:43 -0600

    copy-item doesn't support certificates. Alternate method of copying cert to Personal store

--- letsencrypt-win-simple/Scripts/ImportRDGateway.ps1
+++ letsencrypt-win-simple/Scripts/ImportRDGateway.ps1
@@ -11,9 +11,30 @@ if($CertInStore){
     try{
         # Cert must exist in the personal store of machine to bind to RD Gateway
         if($CertInStore.PSPath -notlike "*LocalMachine\My\*"){
-            $CertInStore = Copy-Item $CertInStore -Destination Cert:\LocalMachine\My -PassThru -ErrorAction Stop
+            $SourceStoreScope = 'LocalMachine'
+            $SourceStorename = $CertInStore.PSParentPath.split("\")[-1]
+
+            $SourceStore = New-Object  -TypeName System.Security.Cryptography.X509Certificates.X509Store  -ArgumentList $SourceStorename, $SourceStoreScope
+            $SourceStore.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadOnly)
+            
+            $cert = $SourceStore.Certificates | Where-Object thumbprint -eq $CertInStore.Thumbprint
+            
+            
+            
+            $DestStoreScope = 'LocalMachine'
+            $DestStoreName = 'My'
+            
+            $DestStore = New-Object  -TypeName System.Security.Cryptography.X509Certificates.X509Store  -ArgumentList $DestStoreName, $DestStoreScope
+            $DestStore.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite)
+            $DestStore.Add($cert)
+            
+            
+            $SourceStore.Close()
+            $DestStore.Close()
+
+            $CertInStore = Get-ChildItem -Path Cert:\LocalMachine\My -Recurse | Where-Object thumbprint -eq $NewCertThumbprint | Sort-Object -Descending | Select-Object -f 1
         }
-        Set-Item -Path RDS:\GatewayServer\SSLCertificate\Thumbprint -Value $NewCertThumbprint -ErrorAction Stop
+        Set-Item -Path RDS:\GatewayServer\SSLCertificate\Thumbprint -Value $CertInStore.Thumbprint -ErrorAction Stop
         "Cert thumbprint set to RD Gateway listener"
     }catch{
         "Cert thumbprint was not set successfully"
