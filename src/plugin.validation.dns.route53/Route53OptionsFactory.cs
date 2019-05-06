19f2223c51dbf5ea28b00946764ca55eae4fc156
Ivan Stus <ivanstus@users.noreply.github.com>
2019-05-06 23:58:24 +0300

    Support for AWS IAM roles added (#1113)

--- src/plugin.validation.dns.route53/Route53OptionsFactory.cs
+++ src/plugin.validation.dns.route53/Route53OptionsFactory.cs
@@ -13,12 +13,18 @@ namespace PKISharp.WACS.Plugins.ValidationPlugins.Dns
         public override Route53Options Aquire(Target target, IArgumentsService arguments, IInputService input, RunLevel runLevel)
         {
             var args = arguments.GetArguments<Route53Arguments>();
-
-            return new Route53Options
+            var options = new Route53Options
             {
-                AccessKeyId = arguments.TryGetArgument(args.Route53AccessKeyId, input, "Access key ID"),
-                SecretAccessKey = arguments.TryGetArgument(args.Route53SecretAccessKey, input, "Secret access key", true)
+                IAMRole = arguments.TryGetArgument(args.Route53IAMRole, input, "AWS IAM role for current EC2 instance (blank for default)")
             };
+
+            if (!string.IsNullOrWhiteSpace(options.IAMRole))
+                return options;
+
+            options.AccessKeyId = arguments.TryGetArgument(args.Route53AccessKeyId, input, "AWS access key ID");
+            options.SecretAccessKey = arguments.TryGetArgument(args.Route53SecretAccessKey, input, "AWS secret access key", true);
+
+            return options;
         }
 
         public override Route53Options Default(Target target, IArgumentsService arguments)
