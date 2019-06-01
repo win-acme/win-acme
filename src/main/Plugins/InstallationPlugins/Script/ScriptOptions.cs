0562119eed7bfbbd930d9b245ad333a579c38255
WouterTinus <wouter.tinus@gmail.com>
2019-06-01 18:02:29 +0200

    Log configured script parameters

--- src/main/Plugins/InstallationPlugins/Script/ScriptOptions.cs
+++ src/main/Plugins/InstallationPlugins/Script/ScriptOptions.cs
@@ -1,5 +1,6 @@
 ﻿using PKISharp.WACS.Plugins.Base;
 using PKISharp.WACS.Plugins.Base.Options;
+using PKISharp.WACS.Services;
 
 namespace PKISharp.WACS.Plugins.InstallationPlugins
 {
@@ -11,5 +12,19 @@ namespace PKISharp.WACS.Plugins.InstallationPlugins
 
         public string Script { get; set; }
         public string ScriptParameters { get; set; }
+
+        /// <summary>
+        /// Show details to the user
+        /// </summary>
+        /// <param name="input"></param>
+        public override void Show(IInputService input)
+        {
+            base.Show(input);
+            input.Show("Script", Script, level: 2);
+            if (!string.IsNullOrEmpty(ScriptParameters))
+            {
+                input.Show("ScriptParameters", ScriptParameters, level: 2);
+            }
+        }
     }
 }
