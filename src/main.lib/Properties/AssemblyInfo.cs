12b79428960b141c0c14d9a96ce9ecdd33bd0a2a
WouterTinus <wouter.tinus@gmail.com>
2019-09-05 08:10:22 +0200

    move more stuff

---
+++ src/main.lib/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyDescription("A simple ACMEv2 client for Windows")]
+[assembly: AssemblyCopyright("Apache 2.0 license")]
+[assembly: AssemblyTrademark("Let's Encrypt is a trademark of ISRG")]
+[assembly: AssemblyCulture("")]
+[assembly: InternalsVisibleTo("wacs")]
+[assembly: InternalsVisibleTo("wacs.test")]
+[assembly: ComVisible(false)]
