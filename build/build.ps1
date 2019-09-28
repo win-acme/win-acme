542f8fcf8ebc7efc5f1cc850848c4e1d51ee477f
WouterTinus <wouter.tinus@gmail.com>
2019-09-28 12:45:20 +0200

    first attempt at CI with .NET Core 3

--- build/build.ps1
+++ build/build.ps1
@@ -5,82 +5,39 @@
 	$ReleaseVersionNumber
 )
 
-function Get-MSBuild-Path {
-    $vs14key = "HKLM:\SOFTWARE\Microsoft\MSBuild\ToolsVersions\14.0"
-    $vs15key = "HKLM:\SOFTWARE\wow6432node\Microsoft\VisualStudio\SxS\VS7"
-    $vs16key = "HKLM:\SOFTWARE\wow6432node\Microsoft\VisualStudio\16.0"
-    $msbuildPath = ""
-
-    if (Test-Path $vs14key) {
-        $key = Get-ItemProperty $vs14key
-        $subkey = $key.MSBuildToolsPath
-        if ($subkey) {
-            $msbuildPath = Join-Path $subkey "msbuild.exe"
-        }
-    }
-
-    if (Test-Path $vs15key) {
-        $key = Get-ItemProperty $vs15key
-        $subkey = $key."15.0"
-        if ($subkey) {
-            $msbuildPath = Join-Path $subkey "MSBuild\15.0\bin\amd64\msbuild.exe"
-        }
-    }
-	
-    if (Test-Path $vs16key) {
-		# TODO: We are supposed to use vswhere here I think
-        $vs2019path = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild.exe"
-		if (Test-Path $vs2019path) {
-			$msbuildPath = $vs2019path
-		}
-    }	
-
-    return $msbuildPath
-}
-
 $PSScriptFilePath = Get-Item $MyInvocation.MyCommand.Path
 $RepoRoot = $PSScriptFilePath.Directory.Parent.FullName
 $BuildFolder = Join-Path -Path $RepoRoot "build"
-$NuGetFolder = Join-Path -Path $RepoRoot "src\packages"
 $SolutionPath = Join-Path -Path $RepoRoot -ChildPath "src\wacs.sln"
 $ProjectRoot = Join-Path -Path $RepoRoot "src\main"
 $Configuration = "Release"
-$MSBuild = Get-MSBuild-Path;
-
-# Go get nuget.exe if we don't have it
-$NuGet = "$BuildFolder\nuget.exe"
-$FileExists = Test-Path $NuGet 
-If ($FileExists -eq $False) {
-	$SourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
-	Invoke-WebRequest $SourceNugetExe -OutFile $NuGet
-}
 
 # Restore NuGet packages
-cmd.exe /c "$NuGet restore $SolutionPath -NonInteractive -PackagesDirectory $NuGetFolder"
+& dotnet restore $ProjectRoot/wacs.csproj
 
 # Set the version number in SolutionInfo.cs
-$NewVersion = 'AssemblyVersion("' + $ReleaseVersionNumber + '")'
-$NewFileVersion = 'AssemblyFileVersion("' + $ReleaseVersionNumber + '")'
-$SolutionInfoPath = Join-Path -Path $ProjectRoot -ChildPath "Properties/AssemblyInfo.cs"
+$SolutionInfoPath = Join-Path -Path $ProjectRoot -ChildPath "wacs.csproj"
 (gc -Path $SolutionInfoPath) `
-	-replace 'AssemblyVersion\("[0-9\.*]+"\)', $NewVersion |
+	-replace '<AssemblyVersion>[0-9\.*]+</AssemblyVersion>', "<AssemblyVersion>$ReleaseVersionNumber</AssemblyVersion>" |
 	sc -Path $SolutionInfoPath -Encoding UTF8
 (gc -Path $SolutionInfoPath) `
-	-replace 'AssemblyFileVersion\("[0-9\.]+"\)', $NewFileVersion |
+	-replace '<Version>[0-9\.*]+</Version>', "<Version>$ReleaseVersionNumber</Version>" |
 	sc -Path $SolutionInfoPath -Encoding UTF8
-		
+(gc -Path $SolutionInfoPath) `
+	-replace '<FileVersion>[0-9\.*]+</FileVersion>', "<FileVersion>$ReleaseVersionNumber</FileVersion>" |
+	sc -Path $SolutionInfoPath -Encoding UTF8	
+
 # Clean solution
-& $MSBuild "$SolutionPath" /p:Configuration=$Configuration /maxcpucount /t:Clean
-if (-not $?)
-{
-	throw "The MSBuild process returned an error code."
-}
+& dotnet clean $ProjectRoot/wacs.csproj -c $Configuration -r win-x64
+& dotnet clean $ProjectRoot/wacs.csproj -c $Configuration -r win-x86
 
 # Build solution
-& $MSBuild "$SolutionPath" /p:Configuration=$Configuration /maxcpucount
+& dotnet publish $ProjectRoot/wacs.csproj -c $Configuration -r win-x64
+& dotnet publish $ProjectRoot/wacs.csproj -c $Configuration -r win-x86
+
 if (-not $?)
 {
-	throw "The MSBuild process returned an error code."
+	throw "The dotnet publish process returned an error code."
 }
 
 ./create-artifacts.ps1 $RepoRoot $ReleaseVersionNumber
--- build/create-artifacts.ps1
+++ build/create-artifacts.ps1
@@ -27,46 +27,55 @@ if (Test-Path $Out)
 }
 New-Item $Out -Type Directory
 
-$MainZip = "win-acme.v$Version.zip"
+# 64bit release
+$MainZip = "win-acme.v$Version.64bit.zip"
 $MainZipPath = "$Out\$MainZip"
-$MainBin = "$Root\src\main\bin\Release"
-
+$MainBin = "$Root\src\main\bin\Release\netcoreapp3.0\win-x64"
 ./sign-exe.ps1 "$MainBin\wacs.exe" "$Root\build\codesigning.pfx" $Password
-
-Copy-Item "$MainBin\wacs.exe" $Temp
-Copy-Item "$MainBin\Web_Config.xml" $Temp
+Copy-Item "$MainBin\publish\wacs.exe" $Temp
 Copy-Item "$MainBin\settings.config" "$Temp\settings_default.config"
-Copy-Item "$MainBin\wacs.exe.config" $Temp
 Copy-Item "$Root\dist\*" $Temp -Recurse
-Set-Content -Path "$Temp\version.txt" -Value "v$Version"
+Set-Content -Path "$Temp\version.txt" -Value "v$Version (64 bit)"
 [io.compression.zipfile]::CreateFromDirectory($Temp, $MainZipPath)
 
+# 32bit release
 Remove-Item $Temp\* -recurse
-$PlugZip = "win-acme.dreamhost.v$Version.zip"
-$PlugZipPath = "$Out\$PlugZip"
-$PlugBin = "$Root\src\plugin.validation.dns.dreamhost\bin\Release\net472"
-Copy-Item "$PlugBin\PKISharp.WACS.Plugins.ValidationPlugins.Dreamhost.dll" $Temp
-[io.compression.zipfile]::CreateFromDirectory($Temp, $PlugZipPath)
+$MainZip = "win-acme.v$Version.32bit.zip"
+$MainZipPath = "$Out\$MainZip"
+$MainBin = "$Root\src\main\bin\Release\netcoreapp3.0\win-x86"
+./sign-exe.ps1 "$MainBin\wacs.exe" "$Root\build\codesigning.pfx" $Password
+Copy-Item "$MainBin\publish\wacs.exe" $Temp
+Copy-Item "$MainBin\settings.config" "$Temp\settings_default.config"
+Copy-Item "$Root\dist\*" $Temp -Recurse
+Set-Content -Path "$Temp\version.txt" -Value "v$Version (32 bit)"
+[io.compression.zipfile]::CreateFromDirectory($Temp, $MainZipPath)
 
-Remove-Item $Temp\* -recurse
-$PlugZip = "win-acme.azure.v$Version.zip"
-$PlugZipPath = "$Out\$PlugZip"
-$PlugBin = "$Root\src\plugin.validation.dns.azure\bin\Release\net472"
-Copy-Item "$PlugBin\Microsoft.Azure.Management.Dns.dll" $Temp
-Copy-Item "$PlugBin\Microsoft.IdentityModel.Clients.ActiveDirectory.dll" $Temp
-Copy-Item "$PlugBin\Microsoft.IdentityModel.Logging.dll" $Temp
-Copy-Item "$PlugBin\Microsoft.IdentityModel.Tokens.dll" $Temp
-Copy-Item "$PlugBin\Microsoft.Rest.ClientRuntime.Azure.Authentication.dll" $Temp
-Copy-Item "$PlugBin\Microsoft.Rest.ClientRuntime.Azure.dll" $Temp
-Copy-Item "$PlugBin\Microsoft.Rest.ClientRuntime.dll" $Temp
-Copy-Item "$PlugBin\PKISharp.WACS.Plugins.ValidationPlugins.Azure.dll" $Temp
-[io.compression.zipfile]::CreateFromDirectory($Temp, $PlugZipPath)
+#Remove-Item $Temp\* -recurse
+#$PlugZip = "win-acme.dreamhost.v$Version.zip"
+#$PlugZipPath = "$Out\$PlugZip"
+#$PlugBin = "$Root\src\plugin.validation.dns.dreamhost\bin\Release\net472"
+#Copy-Item "$PlugBin\PKISharp.WACS.Plugins.ValidationPlugins.Dreamhost.dll" $Temp
+#[io.compression.zipfile]::CreateFromDirectory($Temp, $PlugZipPath)
 
-Remove-Item $Temp\* -recurse
-$PlugZip = "win-acme.route53.v$Version.zip"
-$PlugZipPath = "$Out\$PlugZip"
-$PlugBin = "$Root\src\plugin.validation.dns.route53\bin\Release\net472"
-Copy-Item "$PlugBin\AWSSDK.Core.dll" $Temp
-Copy-Item "$PlugBin\AWSSDK.Route53.dll" $Temp
-Copy-Item "$PlugBin\PKISharp.WACS.Plugins.ValidationPlugins.Route53.dll" $Temp
-[io.compression.zipfile]::CreateFromDirectory($Temp, $PlugZipPath)
+#Remove-Item $Temp\* -recurse
+#$PlugZip = "win-acme.azure.v$Version.zip"
+#$PlugZipPath = "$Out\$PlugZip"
+#$PlugBin = "$Root\src\plugin.validation.dns.azure\bin\Release\net472"
+#Copy-Item "$PlugBin\Microsoft.Azure.Management.Dns.dll" $Temp
+#Copy-Item "$PlugBin\Microsoft.IdentityModel.Clients.ActiveDirectory.dll" $Temp
+#Copy-Item "$PlugBin\Microsoft.IdentityModel.Logging.dll" $Temp
+#Copy-Item "$PlugBin\Microsoft.IdentityModel.Tokens.dll" $Temp
+#Copy-Item "$PlugBin\Microsoft.Rest.ClientRuntime.Azure.Authentication.dll" $Temp
+#Copy-Item "$PlugBin\Microsoft.Rest.ClientRuntime.Azure.dll" $Temp
+#Copy-Item "$PlugBin\Microsoft.Rest.ClientRuntime.dll" $Temp
+#Copy-Item "$PlugBin\PKISharp.WACS.Plugins.ValidationPlugins.Azure.dll" $Temp
+#[io.compression.zipfile]::CreateFromDirectory($Temp, $PlugZipPath)
+
+#Remove-Item $Temp\* -recurse
+#$PlugZip = "win-acme.route53.v$Version.zip"
+#$PlugZipPath = "$Out\$PlugZip"
+#$PlugBin = "$Root\src\plugin.validation.dns.route53\bin\Release\net472"
+#Copy-Item "$PlugBin\AWSSDK.Core.dll" $Temp
+#Copy-Item "$PlugBin\AWSSDK.Route53.dll" $Temp
+#Copy-Item "$PlugBin\PKISharp.WACS.Plugins.ValidationPlugins.Route53.dll" $Temp
+#[io.compression.zipfile]::CreateFromDirectory($Temp, $PlugZipPath)
