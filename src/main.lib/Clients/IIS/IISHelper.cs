9a30e587a685ab4cb1e727ce888f9f47359fc685
Wouter Tinus <win.acme.simple@gmail.com>
2020-02-17 22:09:17 +0100

    avoid code dubplication

--- src/main.lib/Clients/IIS/IISHelper.cs
+++ src/main.lib/Clients/IIS/IISHelper.cs
@@ -79,6 +79,9 @@ namespace PKISharp.WACS.Clients.IIS
                 Where(sb => !string.IsNullOrWhiteSpace(sb.binding.Host)).
                 ToList();
 
+            static string lookupKey(IIISSite site, IIISBinding binding) => 
+                site.Id + "#" + binding.BindingInformation.ToLower();
+
             // Option: hide http bindings when there are already https equivalents
             var https = siteBindings
                 .Where(sb => 
@@ -86,7 +89,7 @@ namespace PKISharp.WACS.Clients.IIS
                     sb.site.Bindings.Any(other => 
                         other.Protocol == "https" &&
                         string.Equals(sb.binding.Host, other.Host, StringComparison.InvariantCultureIgnoreCase)))
-                .ToDictionary(sb => sb.site.Id + "#" + sb.binding.BindingInformation.ToLower());
+                .ToDictionary(sb => lookupKey(sb.site, sb.binding));
 
             var targets = siteBindings.
                 Select(sb => new
@@ -94,7 +97,7 @@ namespace PKISharp.WACS.Clients.IIS
                     host = sb.binding.Host.ToLower(),
                     sb.site,
                     sb.binding,
-                    https = https.ContainsKey(sb.site.Id + "#" + sb.binding.BindingInformation.ToLower())
+                    https = https.ContainsKey(lookupKey(sb.site, sb.binding))
                 }).
                 Select(sbi => new IISBindingOption(sbi.host, _idnMapping.GetAscii(sbi.host))
                 {
