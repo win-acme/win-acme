f15d961ed49a8e3cfac56c711927be82354972eb
Alberto Fustinoni <alberto@aftnet.net>
2020-04-27 14:19:55 +0900

    Build scripts

--- build/build.ps1
+++ build/build.ps1
@@ -27,6 +27,7 @@ $BuildFolder = Join-Path -Path $RepoRoot "build"
 & dotnet publish $RepoRoot\src\plugin.validation.dns.azure\wacs.validation.dns.azure.csproj -c "Release"
 & dotnet publish $RepoRoot\src\plugin.validation.dns.cloudflare\wacs.validation.dns.cloudflare.csproj -c "Release"
 & dotnet publish $RepoRoot\src\plugin.validation.dns.dreamhost\wacs.validation.dns.dreamhost.csproj -c "Release"
+& dotnet publish $RepoRoot\src\plugin.validation.dns.route53\wacs.validation.dns.luadns.csproj -c "Release"
 & dotnet publish $RepoRoot\src\plugin.validation.dns.route53\wacs.validation.dns.route53.csproj -c "Release"
 
 if (-not $?)
