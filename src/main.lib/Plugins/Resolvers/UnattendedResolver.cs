11e19a28d894a8d84919d1be2106c5104d9b9268
Wouter Tinus <wouter.tinus@gmail.com>
2020-07-28 15:17:10 +0200

    error message tweak

--- src/main.lib/Plugins/Resolvers/UnattendedResolver.cs
+++ src/main.lib/Plugins/Resolvers/UnattendedResolver.cs
@@ -99,7 +99,7 @@ namespace PKISharp.WACS.Plugins.Resolvers
                 _log.Error("{n} plugin {x} not available: {m}. " + changeInstructions, 
                     char.ToUpper(className[0]) + className.Substring(1), 
                     defaultOption.plugin?.Name ?? "Unknown",
-                    defaultTypeDisabled.Item2);
+                    defaultTypeDisabled.Item2?.TrimEnd('.'));
                 return nullResult;
             }
 
