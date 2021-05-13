73b8f9fbe7bc109701deb00d8c62c7be2af43c92
Wouter Tinus <wouter.tinus@gmail.com>
2021-05-13 16:28:57 +0200

    implement argumentinputservice for googleclouddns

--- src/plugin.validation.dns.googledns/CloudDnsOptionsFactory.cs
+++ src/plugin.validation.dns.googledns/CloudDnsOptionsFactory.cs
@@ -8,28 +8,35 @@ namespace PKISharp.WACS.Plugins.ValidationPlugins.Dns
 {
     internal sealed class CloudDnsOptionsFactory : ValidationPluginOptionsFactory<CloudDns, CloudDnsOptions>
     {
-        private readonly IArgumentsService _arguments;
+        private readonly ArgumentsInputService _arguments;
 
-        public CloudDnsOptionsFactory(IArgumentsService arguments) : base(Dns01ChallengeValidationDetails.Dns01ChallengeType) => _arguments = arguments;
+        public CloudDnsOptionsFactory(ArgumentsInputService arguments) : base(Dns01ChallengeValidationDetails.Dns01ChallengeType) => _arguments = arguments;
+
+        private ArgumentResult<CloudDnsArguments, string> ServiceAccountKey => _arguments.
+            GetString<CloudDnsArguments>(a => a.ServiceAccountKey).
+            Required();
+
+        private ArgumentResult<CloudDnsArguments, string> ProjectId => _arguments.
+            GetString<CloudDnsArguments>(a => a.ProjectId).
+            Required();
 
         public override async Task<CloudDnsOptions> Aquire(Target target, IInputService input, RunLevel runLevel)
         {
-            var options = new CloudDnsOptions();
-            var args = _arguments.GetArguments<CloudDnsArguments>();
-            options.ServiceAccountKeyPath = await _arguments.TryGetArgument(args.ServiceAccountKey, input, "Path to Service Account Key");
-            options.ProjectId = await _arguments.TryGetArgument(args.ProjectId, input, "Project Id");
-            return options;
+            return new CloudDnsOptions
+            {
+                ServiceAccountKeyPath = await ServiceAccountKey.Interactive(input, "Path to Service Account Key").GetValue(),
+                ProjectId = await ProjectId.Interactive(input).GetValue()
+            };
         }
 
 
-        public override Task<CloudDnsOptions> Default(Target target)
+        public override async Task<CloudDnsOptions> Default(Target target)
         {
-            var args = _arguments.GetArguments<CloudDnsArguments>();
-            return Task.FromResult(new CloudDnsOptions
+            return new CloudDnsOptions
             {
-                ServiceAccountKeyPath = _arguments.TryGetRequiredArgument(nameof(args.ServiceAccountKey), args.ServiceAccountKey),
-                ProjectId = _arguments.TryGetRequiredArgument(nameof(args.ProjectId), args.ProjectId)
-            });
+                ServiceAccountKeyPath = await ServiceAccountKey.GetValue(),
+                ProjectId = await ProjectId.GetValue()
+            };
         }
 
         public override bool CanValidate(Target target) => true;
