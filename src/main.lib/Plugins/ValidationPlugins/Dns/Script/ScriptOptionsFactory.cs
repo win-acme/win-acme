a8503f61ee805ae26b11f7eec50be9ab2f4693f4
Wouter Tinus <wouter.tinus@gmail.com>
2021-05-15 11:09:14 +0200

    add argumentinputservice for dns script

--- src/main.lib/Plugins/ValidationPlugins/Dns/Script/ScriptOptionsFactory.cs
+++ src/main.lib/Plugins/ValidationPlugins/Dns/Script/ScriptOptionsFactory.cs
@@ -11,36 +11,48 @@ namespace PKISharp.WACS.Plugins.ValidationPlugins.Dns
     internal class ScriptOptionsFactory : ValidationPluginOptionsFactory<Script, ScriptOptions>
     {
         private readonly ILogService _log;
-        private readonly IArgumentsService _arguments;
+        private readonly ArgumentsInputService _arguments;
 
         public override bool Match(string name)
         {
-            switch (name.ToLowerInvariant())
+            return name.ToLowerInvariant() switch
             {
-                case "dnsscript":
-                    return true;
-                default:
-                    return base.Match(name);
-            }
+                "dnsscript" => true,
+                _ => base.Match(name),
+            };
         }
 
-        public ScriptOptionsFactory(ILogService log, IArgumentsService arguments) : base(Constants.Dns01ChallengeType)
+        public ScriptOptionsFactory(ILogService log, ArgumentsInputService arguments) : base(Constants.Dns01ChallengeType)
         {
             _log = log;
             _arguments = arguments;
         }
 
+        private ArgumentResult<string?> CommonScript => _arguments.
+            GetString<ScriptArguments>(x => x.DnsScript);
+
+        private ArgumentResult<string?> CreateScript => _arguments.
+            GetString<ScriptArguments>(x => x.DnsCreateScript).
+            Validate(x => Task.FromResult(x.ValidFile(_log)), "invalid file");
+
+        private ArgumentResult<string?> CreateScriptArguments => _arguments.
+            GetString<ScriptArguments>(x => x.DnsCreateScriptArguments).
+            WithDefault(Script.DefaultCreateArguments).
+            DefaultAsNull();
+
+        private ArgumentResult<string?> DeleteScript => _arguments.
+            GetString<ScriptArguments>(x => x.DnsDeleteScript).
+            Validate(x => Task.FromResult(x.ValidFile(_log)), "invalid file");
+
+        private ArgumentResult<string?> DeleteScriptArguments => _arguments.
+            GetString<ScriptArguments>(x => x.DnsDeleteScriptArguments).            
+            WithDefault(Script.DefaultDeleteArguments).
+            DefaultAsNull();
+
         public override async Task<ScriptOptions?> Aquire(Target target, IInputService input, RunLevel runLevel)
         {
-            var args = _arguments.GetArguments<ScriptArguments>();
             var ret = new ScriptOptions();
-            string? createScript = null;
-            do
-            {
-                createScript = args?.DnsCreateScript ?? await input.RequestString("Path to script that creates DNS records");
-            }
-            while (!createScript.ValidFile(_log));
-
+            var createScript = await CreateScript.Interactive(input).GetValue();
             string? deleteScript = null;
             var chosen = await input.ChooseFromMenu(
                 "How to delete records after validation",
@@ -50,66 +62,37 @@ namespace PKISharp.WACS.Plugins.ValidationPlugins.Dns
                         deleteScript = createScript;
                         return Task.CompletedTask;
                     }, "Using the same script"),
-                    Choice.Create<Func<Task>>(async () => {
-                        do {
-                            deleteScript = args?.DnsDeleteScript ??
-                            await input.RequestString("Path to script that deletes DNS records");
-                        }
-                        while (!deleteScript.ValidFile(_log));
-                    }, "Using a different script"),
+                    Choice.Create<Func<Task>>(async () => 
+                        deleteScript = await DeleteScript.Interactive(input).Required().GetValue()
+                    , "Using a different script"),
                     Choice.Create<Func<Task>>(() => Task.CompletedTask, "Do not delete")
                 });
             await chosen.Invoke();
 
             ProcessScripts(ret, null, createScript, deleteScript);
 
+            input.CreateSpace();
             input.Show("{Identifier}", "Domain that's being validated");
             input.Show("{RecordName}", "Full TXT record name");
             input.Show("{Token}", "Expected value in the TXT record");
-            var createArgs = args?.DnsCreateScriptArguments ?? 
-                await input.RequestString($"Input parameters for create script, or enter for default \"{Script.DefaultCreateArguments}\"");
-            string? deleteArgs = null;
-            if (!string.IsNullOrWhiteSpace(ret.DeleteScript) ||
-                !string.IsNullOrWhiteSpace(ret.Script))
+            input.CreateSpace();
+            ret.CreateScriptArguments = await CreateScriptArguments.Interactive(input).GetValue();
+            if (!string.IsNullOrWhiteSpace(ret.DeleteScript) || !string.IsNullOrWhiteSpace(ret.Script))
             {
-                deleteArgs = args?.DnsDeleteScriptArguments ?? 
-                    await input.RequestString($"Input parameters for delete script, or enter for default \"{Script.DefaultDeleteArguments}\"");
+                ret.DeleteScriptArguments = await DeleteScriptArguments.Interactive(input).GetValue();
             }
-            ProcessArgs(ret, createArgs, deleteArgs);
             return ret;
         }
 
         public override async Task<ScriptOptions?> Default(Target target)
         {
-            var args = _arguments.GetArguments<ScriptArguments>();
             var ret = new ScriptOptions();
-            ProcessScripts(ret, args?.DnsScript, args?.DnsCreateScript, args?.DnsDeleteScript);
-            if (!string.IsNullOrEmpty(ret.Script))
-            {
-                if (!ret.Script.ValidFile(_log))
-                {
-                    _log.Error($"Invalid argument --{nameof(args.DnsScript).ToLower()}");
-                    return null;
-                }
-            }
-            else
-            {
-                if (!ret.CreateScript.ValidFile(_log))
-                {
-                    _log.Error($"Invalid argument --{nameof(args.DnsCreateScript).ToLower()}");
-                    return null;
-                }
-                if (!string.IsNullOrEmpty(ret.DeleteScript))
-                {
-                    if (!ret.DeleteScript.ValidFile(_log))
-                    {
-                        _log.Error($"Invalid argument --{nameof(args.DnsDeleteScript).ToLower()}");
-                        return null;
-                    }
-                }
-            }
-
-            ProcessArgs(ret, args?.DnsCreateScriptArguments, args?.DnsDeleteScriptArguments);
+            var commonScript = await CommonScript.GetValue();
+            var createScript = await CreateScript.GetValue();
+            var deleteScript = await DeleteScript.GetValue();
+            ProcessScripts(ret, commonScript, createScript, deleteScript);
+            ret.DeleteScriptArguments = await DeleteScriptArguments.GetValue();
+            ret.CreateScriptArguments = await CreateScriptArguments.GetValue();
             return ret;
         }
 
@@ -133,8 +116,6 @@ namespace PKISharp.WACS.Plugins.ValidationPlugins.Dns
                     _log.Warning("Ignoring --dnsdeletescript because --dnsscript was provided");
                 }
             }
-
-
             if (string.IsNullOrWhiteSpace(commonInput) &&
                 string.Equals(createInput, deleteInput, StringComparison.CurrentCultureIgnoreCase))
             {
@@ -148,24 +129,6 @@ namespace PKISharp.WACS.Plugins.ValidationPlugins.Dns
             {
                 options.CreateScript = string.IsNullOrWhiteSpace(createInput) ? null : createInput;
                 options.DeleteScript = string.IsNullOrWhiteSpace(deleteInput) ? null : deleteInput;
-            }
-        }
-
-        private void ProcessArgs(ScriptOptions options, string? createInput, string? deleteInput)
-        {
-            if (!string.IsNullOrWhiteSpace(createInput) &&
-                createInput != Script.DefaultCreateArguments)
-            {
-                options.CreateScriptArguments = createInput;
-            }
-            if (!string.IsNullOrWhiteSpace(options.DeleteScript) ||
-                !string.IsNullOrWhiteSpace(options.Script))
-            {
-                if (!string.IsNullOrWhiteSpace(deleteInput) &&
-                    deleteInput != Script.DefaultDeleteArguments)
-                {
-                    options.DeleteScriptArguments = deleteInput;
-                }
             }
         }
 
