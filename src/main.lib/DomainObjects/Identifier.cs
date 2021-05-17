0a1826483169a59e0f05d160ad4e7061e166afc4
Wouter Tinus <wouter.tinus@gmail.com>
2021-05-17 12:31:53 +0200

    Make CertificateInfo class understand different types of identifiers

---
+++ src/main.lib/DomainObjects/Identifier.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace PKISharp.WACS.DomainObjects
+{
+    public enum IdentifierType
+    {
+        Unknown,
+        IpAddress,
+        DnsName,
+        UpnName
+    }
+
+    [DebuggerDisplay("{Type}: {Value}")]
+    public class Identifier
+    {
+        public Identifier(string value, IdentifierType identifierType = IdentifierType.Unknown)
+        {
+            Value = value;
+            Type = identifierType;
+        }
+        public IdentifierType Type { get; set; }
+        public string Value { get; set; }
+    }
+}
