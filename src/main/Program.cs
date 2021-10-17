ebb0dff979113e6482ea7e13f2034a25bd550952
Wouter Tinus <win.acme.simple@gmail.com>
2021-10-17 14:33:46 +0200

    test commit

--- src/main/Program.cs
+++ src/main/Program.cs
@@ -43,8 +43,6 @@ namespace PKISharp.WACS.Host
                 return;
             }
 
-
-
             // The main class might change the character encoding
             // save the original setting so that it can be restored
             // after the run.
