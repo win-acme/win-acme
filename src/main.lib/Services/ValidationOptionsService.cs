8708584a51fc63925630257455144cc8d89561e0
Wouter Tinus <win.acme.simple@gmail.com>
2022-03-15 20:56:23 +0100

    remove test value

--- src/main.lib/Services/ValidationOptionsService.cs
+++ src/main.lib/Services/ValidationOptionsService.cs
@@ -1,11 +1,10 @@
 ﻿using PKISharp.WACS.DomainObjects;
 using PKISharp.WACS.Plugins.Base.Options;
-using PKISharp.WACS.Plugins.ValidationPlugins.Http;
 
 namespace PKISharp.WACS.Services
 {
     internal class ValidationOptionsService : IValidationOptionsService
     {
-        public ValidationPluginOptions? GetValidationOptions(Identifier identifier) => new SelfHostingOptions();
+        public ValidationPluginOptions? GetValidationOptions(Identifier identifier) => null; // new SelfHostingOptions();
     }
 }
