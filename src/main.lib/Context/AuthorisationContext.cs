ddcd114373f6ab18c1d02c9f5754ec0a78d63bf4
Wouter Tinus <win.acme.simple@gmail.com>
2022-03-15 20:56:59 +0100

    accept null value for auth details (in case of server error)

--- src/main.lib/Context/AuthorisationContext.cs
+++ src/main.lib/Context/AuthorisationContext.cs
@@ -4,10 +4,10 @@ namespace PKISharp.WACS.Context
 {
     public class AuthorisationContext
     {
-        public Authorization Authorization { get; }
+        public Authorization? Authorization { get; }
         public OrderContext Order { get; }
 
-        public AuthorisationContext(OrderContext order, Authorization authorization)
+        public AuthorisationContext(OrderContext order, Authorization? authorization)
         {
             Order = order;
             Authorization = authorization;
