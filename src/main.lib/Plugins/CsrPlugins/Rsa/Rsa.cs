121e556df8d7d1b8ab4cec74fe0fcf202d921023
Wouter Tinus <win.acme.simple@gmail.com>
2022-07-23 10:18:04 +0200

    lock RSA postprocessing code due to strange errors

--- src/main.lib/Plugins/CsrPlugins/Rsa/Rsa.cs
+++ src/main.lib/Plugins/CsrPlugins/Rsa/Rsa.cs
@@ -13,6 +13,8 @@ namespace PKISharp.WACS.Plugins.CsrPlugins
 {
     internal class Rsa : CsrPlugin<Rsa, RsaOptions>
     {
+        private static readonly object PostProcessLock = new();
+
         public Rsa(
             ILogService log,
             ISettingsService settings,
@@ -42,48 +44,52 @@ namespace PKISharp.WACS.Plugins.CsrPlugins
         [SupportedOSPlatform("windows")]
         public override Task<X509Certificate2> PostProcess(X509Certificate2 original)
         {
-
-            using var privateKey = original.GetRSAPrivateKey();
-            if (privateKey == null)
-            {
-                return Task.FromResult(original);
-            }
-
-            // https://github.com/dotnet/runtime/issues/36899
-            var pwd = Guid.NewGuid().ToString();
-            using var tempRsa = RSA.Create();
-            var pbeParameters = new PbeParameters(PbeEncryptionAlgorithm.Aes256Cbc, HashAlgorithmName.SHA256, 10);
-            tempRsa.ImportEncryptedPkcs8PrivateKey(pwd, privateKey.ExportEncryptedPkcs8PrivateKey(pwd, pbeParameters), out var read);
-
-            try
-            {
-                var cspParameters = new CspParameters
+            // Lock this section of code because we have seen
+            // exceptions and errors in heavy threading scenarios,
+            // which might be an underlying framework/OS bug
+            lock (PostProcessLock) {
+                using var privateKey = original.GetRSAPrivateKey();
+                if (privateKey == null)
                 {
-                    KeyContainerName = Guid.NewGuid().ToString(),
-                    KeyNumber = 1,
-                    Flags = CspProviderFlags.NoPrompt,
-                    ProviderType = 12 // Microsoft RSA SChannel Cryptographic Provider
-                };
-                var rsaProvider = new RSACryptoServiceProvider(cspParameters);
-                var parameters = tempRsa.ExportParameters(true);
-                rsaProvider.ImportParameters(parameters);
+                    return Task.FromResult(original);
+                }
 
-                var tempPfx = new X509Certificate2(
-                    original.Export(X509ContentType.Cert),
-                    "", 
-                    X509KeyStorageFlags.EphemeralKeySet | X509KeyStorageFlags.Exportable);
-                tempPfx = tempPfx.CopyWithPrivateKey(rsaProvider);
-                return Task.FromResult(tempPfx);
-            }
-            catch (Exception ex)
-            {
-                // If we couldn't convert the private key that 
-                // means we're left with a pfx generated with the
-                // 'wrong' Crypto provider therefor delete it to 
-                // make sure it's retried on the next run.
-                _log.Warning("Error converting private key to Microsoft RSA SChannel Cryptographic Provider, which means it might not be usable for Exchange 2013.");
-                _log.Verbose("{ex}", ex);
-                throw;
+                // https://github.com/dotnet/runtime/issues/36899
+                var pwd = Guid.NewGuid().ToString();
+                using var tempRsa = RSA.Create();
+                var pbeParameters = new PbeParameters(PbeEncryptionAlgorithm.Aes256Cbc, HashAlgorithmName.SHA256, 10);
+                tempRsa.ImportEncryptedPkcs8PrivateKey(pwd, privateKey.ExportEncryptedPkcs8PrivateKey(pwd, pbeParameters), out var read);
+
+                try
+                {
+                    var cspParameters = new CspParameters
+                    {
+                        KeyContainerName = Guid.NewGuid().ToString(),
+                        KeyNumber = 1,
+                        Flags = CspProviderFlags.NoPrompt,
+                        ProviderType = 12 // Microsoft RSA SChannel Cryptographic Provider
+                    };
+                    var rsaProvider = new RSACryptoServiceProvider(cspParameters);
+                    var parameters = tempRsa.ExportParameters(true);
+                    rsaProvider.ImportParameters(parameters);
+
+                    var tempPfx = new X509Certificate2(
+                        original.Export(X509ContentType.Cert),
+                        "",
+                        X509KeyStorageFlags.EphemeralKeySet | X509KeyStorageFlags.Exportable);
+                    tempPfx = tempPfx.CopyWithPrivateKey(rsaProvider);
+                    return Task.FromResult(tempPfx);
+                }
+                catch (Exception ex)
+                {
+                    // If we couldn't convert the private key that 
+                    // means we're left with a pfx generated with the
+                    // 'wrong' Crypto provider therefor delete it to 
+                    // make sure it's retried on the next run.
+                    _log.Warning("Error converting private key to Microsoft RSA SChannel Cryptographic Provider, which means it might not be usable for Exchange 2013.");
+                    _log.Verbose("{ex}", ex);
+                    throw;
+                }
             }
         }
 
